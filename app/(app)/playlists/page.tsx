bf3c50a10c3b2348317874bcbf99b72eef017e3c
heyjhonny <ramjonathan@gmail.com>
2026-08-19 21:02:05 +0200

    fix: lint alphabetical import

--- app/(app)/playlists/page.tsx
+++ app/(app)/playlists/page.tsx
@@ -1,10 +1,10 @@
 import { connection } from "next/server";
 import { Suspense } from "react";
-import { listAllRecipes } from "@/lib/recipes/catalog";
 import { PageTemplate } from "@/components/common/page-template";
 import DbNotConfiguredErrorCard from "@/components/error-cards/db-not-configured-error-card";
 import { getDbStatus } from "@/lib/database/utils";
 import { getInitData } from "@/lib/getInitData";
+import { listAllRecipes } from "@/lib/recipes/catalog";
 import PlaylistsClientPage from "./client-page";
 
 export const metadata = {
